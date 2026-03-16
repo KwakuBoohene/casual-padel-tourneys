2c4385fb6179407c5f0e94d7226483a46e290823
kwaku.boohene <kwaku.boohene@everykey.com>
2026-03-16 23:39:33 +0000

    Update app name to "Casual Padel Tourneys" in multiple files

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,10 +1,12 @@
-import { SafeAreaView, StatusBar } from "react-native";
+import { Platform, SafeAreaView, StatusBar } from "react-native";
 
 import { OrganizerScreen } from "./screens/OrganizerScreen/OrganizerScreen";
 
 export function App() {
+  const androidTopInset = Platform.OS === "android" ? (StatusBar.currentHeight ?? 0) : 0;
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#0f172a" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0f172a", paddingTop: androidTopInset }}>
       <StatusBar barStyle="light-content" />
       <OrganizerScreen />
     </SafeAreaView>
